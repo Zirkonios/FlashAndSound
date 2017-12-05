aefed59a6519fdfd40f0185bba60d0245c2bd4ce
Orfeas Athanassiou <sdi1100020@di.uoa.gr>
2017-12-05 16:25:01 +0200

    plz work

--- app/src/main/java/com/anaptixi/flashandsound/SettingsActivity.java
+++ app/src/main/java/com/anaptixi/flashandsound/SettingsActivity.java
@@ -2,12 +2,29 @@ package com.anaptixi.flashandsound;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.ClipboardManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Button retButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+    public void takeIp(View view) {
+
+    }
+
+    retButton = (Button) findViewById(R.id.retButton);
+        retButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            return;
+        }
+    });
 }
